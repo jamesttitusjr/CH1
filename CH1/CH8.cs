4d7b0fe633f458e816b204dddd5a8a40589a893b
James Titus <jamesttitusjr@outlook.com>
2022-08-31 15:52:47 -0400

    Add project files.

---
+++ CH1/CH8.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CH1
+{
+    internal class CH8
+    {
+        
+    }
+}
+// Pg. 337
+// namespace System.Collection.Generic
+// {
+// [DefaultMember("Item")] aka this indexer
+// public interface Ilist<T> : ICollection>, IEnumerable<T>, IEnumerable
+// {
+// T this[int index] { get; set; }
+// int IndexOf(T item);
+// void Insert(int index, T item);
+// void RemoveAt(int index);
+// }
+// }
+
